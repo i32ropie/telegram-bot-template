420478e20ff70629bc577b402f6f03115cc7f89e
eduroldanpijuan <eduroldanpijuan@gmail.com>
2024-06-08 11:37:45 +0000

    Config para usar idx

---
+++ .idx/dev.nix
@@ -0,0 +1,32 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  services.docker.enable=true;
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.docker
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "ms-python.python"
+      "ms-azuretools.vscode-docker"
+      "ms-python.debugpy"
+    ];
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when the workspace is (re)started
+      onStart = {
+        docker-build = "docker compose build";
+      };
+    };
+  };
+}
